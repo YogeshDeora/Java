772b98668b37296142af826c4e0242b545c2d66f
Yogesh Deora <yogeshdeora428@gmail.com>
2025-01-05 04:39:24 +0530

    Reverse.java is added

---
+++ reverse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class reverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        int reversedNumber = reverseNumber(number);
+        System.out.println("Reversed number: " + reversedNumber);
+    }
+    
+    public static int reverseNumber(int number) {
+        int reversed = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return reversed;
+    }
+}
